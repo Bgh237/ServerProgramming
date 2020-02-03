b871efeff9928f97883185087b1072361b5b6d5e
Barry Simon <bgh237@myy.haaga-helia.fi>
2020-02-03 16:21:18 +0200

    First commit

---
+++ src/main/java/com/example/Bookstore/domain/Book.java
@@ -0,0 +1,44 @@
+package com.example.Bookstore.domain;
+
+public class Book {
+	private String title, author, isbn;
+	private double price;
+	private int year;
+	
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
+}
